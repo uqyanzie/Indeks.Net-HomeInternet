8bb67344e983a6808e79711b39d86fbefc940c46
Hasya <uqyanzie@gmail.com>
2021-12-24 16:00:35 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+typedef struct{
+	char ;
+	int harga;
+}produk;
+
+typedef struct{
+	char ID[8];
+	char password[15];
+	produk layanan; 
+	int bill;
+}Dt_costumer;
+
+int main()
+{
+	cout<<" ============================================================================================= "<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"       IIIIIII   NNN        NNN  DDDDD        EEEEEEEEEE    KKK    KK   SSSSSSSSSSS" <<endl;
+	cout<<"         III     NNNN       NNN	 DDDDDD       EEEEEEEEEE    KKK   KK    SSS"        <<endl;
+	cout<<"         III     NNNNNN     NNN	 DDD  DD      EEE           KKK  KK      SSS"       <<endl;
+	cout<<"         III     NNNNNNN    NNN	 DDD   DD     EEE           KKK KK        SSS"      <<endl;
+	cout<<"         III     NNN  NNN   NNN	 DDD	DD    EEEEEE        KKKKK          SSS"     <<endl;
+	cout<<"         III     NNN   NNN  NNN	 DDD 	 DD   EEEEEE        KKK KK          SSSSSSS"<<endl;
+	cout<<"         III     NNN    NNNNNNN	 DDD 	DD    EEE           KKK  KK             SSS"<<endl;
+	cout<<"         III     NNN      NNNNN	 DDD   DD     EEE           KKK   KK           SSS" <<endl;
+	cout<<"         III     NNN       NNNN	 DDD  DD      EEEEEEEEEE    KKK    KK	      SSS"  <<endl;
+	cout<<"       IIIIIII   NNN        NNN	 DDDDDD       EEEEEEEEEE    KKK     KK	 SSSSSSS"   <<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<" ============================================================================================== "<<endl;
+	cout<<endl;
+	cout<<endl;
+	cout<<"    SELAMAT DATANG DI LAYANAN INDEKS.NET"<<endl;
+	cout<<endl;
+	cout<<"    1. Daftar Berlangganan"<<endl;
+	cout<<"    2. Upgrade Layanan"    <<endl;
+	cout<<"    3. Cek Tagihan "       <<endl;
+	cout<<"    4. Berhenti Langganan" <<endl;
+	cout<<"    5. Akses Sebagai Admin"<<endl;
+
+}
